08e5e138545ab7e8ce9bf2cd04af400ed10c42cc
Jon Alm Eriksen <jon.alm.eriksen@gmail.com>
2016-04-06 23:55:18 +0200

    add distribution method, rename types

--- src/metabias.jl
+++ src/metabias.jl
@@ -1,5 +1,5 @@
 module MetaBias
-export Null, MixModel, reset_τ, import_sampledata
+export NullLikelihoodPrior,MixModelLikelihoodPrior, NullPosterior, reset_τ, import_sampledata, density
 
 #using Base.length
 using Distributions: Normal
@@ -15,11 +15,11 @@ immutable NullDensityParam
     τ::Float64
 end
 
-type Null <: LikelihoodPrior
+type NullLikelihoodPrior <: LikelihoodPrior
     ndp::NullDensityParam
 end
 
-type MixModel <: LikelihoodPrior
+type MixModelLikelihoodPrior <: LikelihoodPrior
     ndp::NullDensityParam
     z₁::Array{Float64,1} # significant z values
     σ₁::Array{Float64,1} # significant σ = sqrt(var))
@@ -49,14 +49,14 @@ function reset_τ(lp::LikelihoodPrior,τ)
     lp
 end
 
-function Null{S<:Real,T<:Real}(z::Array{S,1},var::Array{T,1},τ::Float64=2.0)
-    Null(NullDensityParam(z,var,τ))
+function NullLikelihoodPrior{S<:Real,T<:Real}(z::Array{S,1},var::Array{T,1},τ::Float64=2.0)
+    NullLikelihoodPrior(NullDensityParam(z,var,τ))
 end
 
-function MixModel{S<:Real,T<:Real}(z::Array{S,1},var::Array{T,1},τ::Float64=2.0,Z::Float64=1.96)
+function MixModelLikelihoodPrior{S<:Real,T<:Real}(z::Array{S,1},var::Array{T,1},τ::Float64=2.0,Z::Float64=1.96)
     σ = sqrt(var)
     idx =  abs(z) .> (σ * Z)
-    MixModel(NullDensityParam(z,var,τ), z[idx],σ[idx],length(idx)-sum(idx),Z)
+    MixModelLikelihoodPrior(NullDensityParam(z,var,τ), z[idx],σ[idx],length(idx)-sum(idx),Z)
 end
 
 function Ni(μ, σ, Z)
@@ -65,15 +65,20 @@ function Ni(μ, σ, Z)
 end
 
 density(ndp::NullDensityParam,x::Real) = ndp.a * exp(-(ndp.b*x^2 - ndp.c*x + ndp.d))
-density(null::Null,μ::Real) = density(null.ndp,μ)
-function density{T<:Real}(mm::MixModel, x::Array{T,1})
+density(null::NullLikelihoodPrior,μ::Real) = density(null.ndp,μ) # 
+function density{T<:Real}(mm::MixModelLikelihoodPrior, x::Array{T,1})
     @assert length(x) == 2
     η, μ = x[1], x[2]
     modification = reduce(*,[η + (1-η)*Ni(μ,σᵢ,mm.Z) for σᵢ in mm.σ₁]) + η^mm.N₀
     density(mm.npd,μ) * modification
 end
 
-#NormalizedNullDistribution(z,) = Distributions.Normal()
+function NullPosterior{S<:Real,T<:Real}(z::Array{S,1},var::Array{T,1},τ::Real)
+    postvar = 1 /(sum(1./var) + 1./τ)
+    postmean = postvar * sum(z./var)
+    Normal(postmean, sqrt(postvar))
+end
+
 #density(::LikelihoodPrior)
 
 function import_sampledata(h5file, name)
--- test/runtest.jl
+++ test/runtest.jl
@@ -1,25 +1,40 @@
 using MetaBias
 using FactCheck
+using Distributions
 
+srand(42) # fixed random seed
+
+# data used in tests
 zi, vi = import_sampledata("../data/sampledata.hdf5", "/konstantopoulos");
+r = randn(5)
 
 facts("abcd parameters equal in Null and MixModel") do
-    lp0 = Null(zi,vi)
-    lp1 = MixModel(zi,vi)
+    lp0 = NullLikelihoodPrior(zi,vi)
+    lp1 = MixModelLikelihoodPrior(zi,vi)
     @fact lp0.ndp --> lp1.ndp
 end
 
 facts("equal default hyper parameter τ in Null and MixModel") do
-    lp0 = Null(zi,vi)
-    lp1 = MixModel(zi,vi)
+    lp0 = NullLikelihoodPrior(zi,vi)
+    lp1 = MixModelLikelihoodPrior(zi,vi)
+    @fact lp0.ndp.τ --> lp1.ndp.τ
     @fact lp0.ndp.τ --> lp1.ndp.τ
 end
 
 facts("updating τ is consistent with calculating from scratch") do
-    for LP in (MixModel,)
+    for LP in (NullLikelihoodPrior,MixModelLikelihoodPrior)
         lp0 = LP(zi,vi,2.0)
         lp0 = reset_τ(lp0, 3.0)
         lp1 = LP(zi,vi,3.0)
         @fact lp0.ndp --> lp1.ndp
     end
 end
+
+facts("Null likelihod prior proportioal to density") do
+    τ = 3.0
+    nullposterior = NullPosterior(zi,vi,τ)
+    display(pdf(nullposterior, r))
+    #nulllikelihoodprior = 
+    #@fact lp0.ndp --> lp1.ndp
+end
+
