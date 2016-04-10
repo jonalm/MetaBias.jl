797dc7d384349abc263a328b40d1652be183852c
Jon Alm Eriksen <jon.alm.eriksen@gmail.com>
2016-04-10 23:37:10 +0200

    add MixModel, rename Ni => norm_const

--- src/metabias.jl
+++ src/metabias.jl
@@ -1,10 +1,35 @@
 module MetaBias
-export NullLikelihoodPrior,MixModelLikelihoodPrior, NullPosterior, reset_τ, import_sampledata, density
+export NullLikelihoodPrior,MixModelLikelihoodPrior, MixModel, NullPosterior, reset_τ, import_sampledata, density, length
 
-using Distributions: Normal, cdf
+using Distributions: Normal, Bernoulli, cdf, ContinuousUnivariateDistribution, @check_args
 using HDF5: h5open
 
+# extending
+import Base: length
+import Distributions: rand, pdf
+
 abstract LikelihoodPrior
+
+immutable MixModel <: ContinuousUnivariateDistribution
+    σ::Float64
+    Z::Float64
+    bd::Bernoulli
+    nd::Normal
+
+    function MixModel(η::Real, μ::Real, σ2::Real, Z::Real)
+        @check_args(MixModel, zero(η)<= η <= one(η) && zero(σ2) < σ2  && zero(Z) < Z)
+        σ = sqrt(σ2)
+        new(σ, Z, Bernoulli(η), Normal(μ,σ))
+    end
+end
+MixModel(η::Real, μ::Real, σ2::Real) = MixModel(η, μ, σ2, 1.96)
+pdf(d::MixModel,x) = pdf(d.nd,x) * (d.bd.p + (1-d.bd.p)*norm_const(nd,d.σ,d.Z)*(x > d.Z*d.σ))
+
+function rand(d::MixModel)
+    honest = rand(d.bd)
+    x = rand(d.nd)
+    honest ? x : (d.Z*d.σ < x ? x : rand(d)) 
+end
 
 immutable NullDensityParam
     a::Float64
@@ -12,18 +37,6 @@ immutable NullDensityParam
     c::Float64
     d::Float64
     τ::Float64
-end
-
-type NullLikelihoodPrior <: LikelihoodPrior
-    ndp::NullDensityParam
-end
-
-type MixModelLikelihoodPrior <: LikelihoodPrior
-    ndp::NullDensityParam
-    z₁::Array{Float64,1} # significant z values
-    σ₁::Array{Float64,1} # significant σ = sqrt(var))
-    N₀::Int64 # number of non significant data, lenght(z) = length(z₁) + N₀
-    Z::Float64 # z-score corresponding to test significance level
 end
 
 function NullDensityParam{S<:Real,T<:Real}(z::Array{S,1},var::Array{T,1},τ::Real)
@@ -37,19 +50,20 @@ function NullDensityParam{S<:Real,T<:Real}(z::Array{S,1},var::Array{T,1},τ::Rea
     NullDensityParam(a,b,c,d,τ)
 end
 
-function reset_τ(ndp::NullDensityParam, τ)
-    a = ndp.a * sqrt(ndp.τ) / sqrt(τ)
-    b = ndp.b - 1.0/(2ndp.τ) + 1.0/(2τ)
-    NullDensityParam(a,b,ndp.c,ndp.d,τ)
-end
-
-function reset_τ(lp::LikelihoodPrior,τ)
-    lp.ndp = reset_τ(lp.ndp, τ)
-    lp
+type NullLikelihoodPrior <: LikelihoodPrior
+    ndp::NullDensityParam
 end
 
 function NullLikelihoodPrior{S<:Real,T<:Real}(z::Array{S,1},var::Array{T,1},τ::Float64=2.0)
     NullLikelihoodPrior(NullDensityParam(z,var,τ))
+end
+
+type MixModelLikelihoodPrior <: LikelihoodPrior
+    ndp::NullDensityParam
+    z₁::Array{Float64,1} # significant z values
+    σ₁::Array{Float64,1} # significant σ = sqrt(var))
+    N₀::Int64 # number of non significant data, lenght(z) = length(z₁) + N₀
+    Z::Float64 # z-score corresponding to test significance level
 end
 
 function MixModelLikelihoodPrior{S<:Real,T<:Real}(z::Array{S,1},var::Array{T,1},τ::Float64=2.0,Z::Float64=1.96)
@@ -58,9 +72,23 @@ function MixModelLikelihoodPrior{S<:Real,T<:Real}(z::Array{S,1},var::Array{T,1},
     MixModelLikelihoodPrior(NullDensityParam(z,var,τ), z[idx],σ[idx],length(idx)-sum(idx),Z)
 end
 
-function Ni(μ, σ, Z)
+function reset_τ(ndp::NullDensityParam, τ)
+    a = ndp.a * sqrt(ndp.τ) / sqrt(τ)
+    b = ndp.b - 1.0/(2ndp.τ) + 1.0/(2τ)
+    NullDensityParam(a,b,ndp.c,ndp.d,τ)
+end
+function reset_τ(lp::LikelihoodPrior,τ)
+    lp.ndp = reset_τ(lp.ndp, τ)
+    lp
+end
+
+function norm_const(nd::Normal, σ::Real, Z::Real)
+    @assert nd.σ == σ
+    1/(1.0 - cdf(nd,σ*Z) + cdf(nd,-σ*Z))
+end
+function norm_const(μ::Real, σ::Real, Z::Real)
     nd = Normal(μ, σ)
-    1.0 - cdf(nd,σ*Z) + cdf(nd,-σ*Z)
+    norm_const(nd, σ, Z)
 end
 
 density(ndp::NullDensityParam,x::Real) = ndp.a * exp(-(ndp.b*x^2 - ndp.c*x + ndp.d))
@@ -76,9 +104,9 @@ end
 
 function density{T<:Real}(mm::MixModelLikelihoodPrior, x::Array{T,1})
     @assert length(x) == 2
-    η, μ = x[1], x[2]
+    η, μ, σ₁, N₀, Z = x[1], x[2], mm.σ₁, mm.N₀, mm.Z
     @assert 0. <= η <= 1.
-    modification = reduce(*,[η + (1-η)*Ni(μ,σᵢ,mm.Z) for σᵢ in mm.σ₁]) * η^mm.N₀
+    modification = reduce(*,[η + (1-η)*norm_const(μ,σᵢ,Z) for σᵢ in σ₁]) * η^N₀
     density(mm.ndp,μ) * modification
 end
 function density{T<:Real}(mm::MixModelLikelihoodPrior, x::Array{T,2})
@@ -96,7 +124,6 @@ function NullPosterior{S<:Real,T<:Real}(z::Array{S,1},var::Array{T,1},τ::Real=2
     Normal(postmean, sqrt(postvar))
 end
 
-
 function import_sampledata(h5file, name)
     out = h5open(h5file, "r") do file
         zi = read(file, name*"/zi")[:]
@@ -105,6 +132,4 @@ function import_sampledata(h5file, name)
     end
     return out
 end
-
-
 end
