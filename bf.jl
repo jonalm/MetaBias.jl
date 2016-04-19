84eb749169c4217e6a4df1d0ac34cd6b369e0f62
Jon Alm Eriksen <jon.alm.eriksen@gmail.com>
2016-04-19 11:19:00 +0200

    add bayes factor on testdata

---
+++ bf.jl
@@ -0,0 +1,24 @@
+using MetaBias
+using HDF5
+using Formatting
+
+h5fn = "data/sampledata.hdf5"
+
+namevec = h5open(h5fn, "r") do file
+    names(file)
+end
+
+header = lpad("dataset", 16," ")*" |  Bayes Factor   "
+println()
+println(header)
+println("-"^length(header))
+for name in namevec
+    zi, vi = import_sampledata(h5fn, name)
+    mm = MixModelLikelihoodPrior(zi, vi)
+    bf = bayesfactor(mm)
+    println(lpad(name, 16," ")*" | "*sprintf1("%10.3f", bf ))
+end
+println()
+
+
+
